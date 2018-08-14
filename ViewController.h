c881aa2b5ba5d99aaf3280ac0c100d5cdb81a9c1
Nirosha <38420481+Nirosha-iOS@users.noreply.github.com>
2018-08-14 16:29:27 +0530

    Delete ViewController.h

--- ViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "DetailViewController.h"
-
-@interface ViewController : UIViewController<NSFetchedResultsControllerDelegate>
-@property (nonatomic, strong) IBOutlet UITableView *tableView;
-@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
-@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
-@end
